143130121134564071b3c23b16de842095a7ecf2
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-08-26 04:25:18 +0300

    Dynamic word counter

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 def index(request):
     name = 'Tamir'
     return render(request, 'index.html', {'name': name})
+
+
+def counter(request):
+    text = request.GET['text']
+    number_of_words = len(text.split())
+    context = {
+        'number_of_words': number_of_words
+    }
+    return render(request, 'counter.html', context)
