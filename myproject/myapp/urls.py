7d9cdfdcdedcd7d6ef12bd6ddc92c98a2f76f6a4
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-08-26 03:57:38 +0300

    Simple working Django project

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name = 'index') 
+    # The first argument is the actual route in the site, the url. The second will get the function to return and the last is the name of the page.
+]
