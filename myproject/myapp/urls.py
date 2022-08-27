a159f00a611bdf8d8340132122546bfdcfdf9e09
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-08-28 00:17:18 +0300

    User registration

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.main_page, name = 'index'),
     # The first argument is the actual route in the site, the url. The second will get the function to return and the last is the name of the page.
-    path('counter', views.counter, name = 'counter')
+    path('counter', views.counter, name = 'counter'),
+    path('register', views.register, name = 'register')
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Feature, Doctor
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 def index(request):
     name = 'Tamir'
     return render(request, 'index.html', {'name': name})
 
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(email = email).exists():
+                messages.info(request, 'Email already in use')
+                return redirect('register')
+            elif User.objects.filter(username = username).exists():
+                messages.info(request, 'Username already in use')
+                return redirect('register')
+            user = User.objects.create_user(username = username, email = email, password = password)
+            user.save()
+            return redirect('login')
+        else:
+            messages.info(request, "Passwords inputted were not the same")
+    return render(request, 'register.html')
 
 def main_page(request):
     features = Feature.objects.all()
